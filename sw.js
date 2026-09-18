638e9f19d40eb0bc210e587a755a035be1d1d885
telatiro <telatiro.pruebas@gmail.com>
2026-09-18 17:12:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // TeLaTiro - Service Worker para PWA (Instalación Standalone en Android e iOS)
-const CACHE_NAME = 'telatiro-pwa-v24';
+const CACHE_NAME = 'telatiro-pwa-v25';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
